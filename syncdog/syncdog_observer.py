222bb5bf6a7ea2e0f8d371d6068a5881191b1289
toann1980 <28768308+toann1980@users.noreply.github.com>
2024-10-16 21:43:59 -0700

    Docs: added docstrings

--- syncdog/syncdog_observer.py
+++ syncdog/syncdog_observer.py
@@ -21,6 +21,24 @@ class SyncDogObserver:
         self._is_running = False
 
     def change_directory(self, new_directory: Path | str) -> None:
+        """
+        Changes the current working directory to the specified new directory.
+
+        Args:
+            new_directory (Path | str): The new directory to change to. It can
+                be either a Path object or a string representing the path.
+
+        Returns:
+            None
+
+        Logs:
+            DEBUG: Logs the directory change from the current directory to the
+                new directory.
+
+        Notes:
+            If the observer is currently running, it will be stopped before
+                changing the directory.
+        """
         logger.debug(
             f"Changing directory from {self.directory} to {new_directory}"
         )
@@ -29,6 +47,15 @@ class SyncDogObserver:
         self.directory = new_directory
 
     def run(self) -> None:
+        """
+        Starts the observer to monitor the directory for changes.
+
+        Schedules the handler, starts the observer, and sets the running flag.
+        Handles KeyboardInterrupt to stop the observer gracefully.
+
+        Raises:
+            KeyboardInterrupt: If interrupted by a keyboard signal.
+        """
         self.observer.schedule(self.handler, self.directory, recursive=True)
         self.observer.start()
         self._is_running = True
@@ -40,9 +67,21 @@ class SyncDogObserver:
             self.observer.join()
 
     def stop(self) -> None:
+        """
+        Stops the observer and waits for it to finish.
+
+        This method stops the observer thread and waits for it to terminate,
+        then sets the running flag to False.
+        """
         self.observer.stop()
         self.observer.join()
         self._is_running = False
 
     def is_running(self) -> bool:
+        """
+        Check if the observer is currently running.
+
+        Returns:
+            bool: True if the observer is running, False otherwise.
+        """
         return self._is_running
