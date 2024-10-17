02c6b350852532ffcc29c8d88f8ae88d10be6566
toann1980 <28768308+toann1980@users.noreply.github.com>
2024-10-17 12:43:17 -0700

    Refactor: bsdiff_file_handler renamed to syncdog_file_handler

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PySide6.QtWidgets import QApplication
 
 from syncdog.syncdog_window import SyncFilesWindow
 from syncdog.syncdog_observer import SyncDogObserver
-from syncdog.bsdiff_file_handler import SyncDogFileHandler
+from syncdog.syncdog_file_handler import SyncDogFileHandler
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
