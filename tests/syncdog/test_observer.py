4f7c1ffe0d369c89988a1fb6f01b5edf9e920dc6
toann1980 <28768308+toann1980@users.noreply.github.com>
2024-10-24 21:49:30 -0700

    Refactor: Update observer based on new method names

--- tests/syncdog/test_observer.py
+++ tests/syncdog/test_observer.py
@@ -15,7 +15,7 @@ class TestSyncDogObserver(unittest.TestCase):
         self.destination = Path(tempfile.mkdtemp())
         self.handler = FileSystemEventHandler()
         self.observer = SyncDogObserver(
-            directory=self.source, file_handler=self.handler)
+            directory=self.source, handler=self.handler)
         self.thread = threading.Thread(target=self.observer.run)
 
     def test_run(self):
@@ -24,30 +24,57 @@ class TestSyncDogObserver(unittest.TestCase):
         self.thread.join(timeout=1)
         self.assertTrue(self.observer.is_running)
 
-    def test_stop(self):
+    def test_run_multiple_directories(self):
+        self.observer.set_directory([self.source, self.destination])
         self.thread.start()
         # Allow some time for the observer to start
+        self.thread.join(timeout=1)
+        self.assertTrue(self.observer.is_running)
+        self.assertTrue(
+            self.observer.directory, [self.source, self.destination]
+        )
+
+    def test_stop(self):
+        self.thread.start()
         self.observer.stop()
         self.thread.join(timeout=1)
         self.assertFalse(self.observer.is_running)
 
-    def test_change_directory(self):
+    def test_set_directory(self):
         self.thread.start()
         self.assertEqual(self.observer.directory, self.source)
         self.observer.stop()
         self.thread.join(timeout=1)
-        self.observer.change_directory(self.destination)
+        self.observer.set_directory(self.destination)
         self.assertEqual(self.observer.directory, self.destination)
 
-    def test_change_directory_error(self):
+    def test_set_directoryy_error(self):
         self.thread.start()
         self.assertEqual(self.observer.directory, self.source)
         self.thread.join(timeout=1)
         with self.assertRaises(RuntimeError):
-            self.observer.change_directory(self.destination)
+            self.observer.set_directory(self.destination)
 
         self.assertEqual(self.observer.directory, self.source)
         print(f'observer is_running: {self.observer.is_running}')
+
+    def test_set_handler(self):
+        self.thread.start()
+        self.assertEqual(self.observer.handler, self.handler)
+        self.observer.stop()
+        self.thread.join(timeout=1)
+        new_handler = FileSystemEventHandler()
+        self.observer.set_handler(new_handler)
+        self.assertEqual(self.observer.handler, new_handler)
+
+    def test_set_handler_when_running(self):
+        self.thread.start()
+        self.assertEqual(self.observer.handler, self.handler)
+        self.thread.join(timeout=1)
+        with self.assertRaises(RuntimeError):
+            self.observer.set_handler(FileSystemEventHandler())
+
+        self.assertEqual(self.observer.handler, self.handler)
 
     def test_repr(self):
         exp_repr = f"SyncDogObserver(directory={self.observer.directory!r}, " \
